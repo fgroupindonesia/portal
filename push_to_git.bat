25e52972a8378d855197e8ab4c492b9c15ad5cc9
fgroupindonesia <gumuruh@fgroupindonesia.com>
2021-08-28 14:53:38 +0700

    Updating the Exam Questions logic with error solved under SWThreadWorker

--- push_to_git.bat
+++ push_to_git.bat
@@ -1,4 +1,4 @@
-git remote set-url origin https://github.com/fgroupindonesia/portal.git
+git remote set-url origin https://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@github.com/fgroupindonesia/portal.git
 git add .
 git commit -m "Updating the Exam Questions logic with error solved under SWThreadWorker"
 git push -u origin main
