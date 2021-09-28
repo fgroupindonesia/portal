46671bfffa49d276e1479e392d677644383ac6e3
fgroupindonesia <gumuruh@fgroupindonesia.com>
2021-09-28 13:17:02 +0700

    updating Client Frame with Exam UI.

--- push_to_git.bat
+++ push_to_git.bat
@@ -1,6 +1,6 @@
-git remote set-url origin https://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@github.com/fgroupindonesia/portal.git
+git remote set-url origin https://github.com/fgroupindonesia/portal.git
 git add .
-git commit -m "Exam Questions are ready with some error under Exam Student Answer logic."
+git commit -m "updating Client Frame with Exam UI."
 git push origin main
 git gc --prune
 git pull
